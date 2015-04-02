33b83f436c8089e098a499fcb686734e534679ec
Raül Pérez <repejota@gmail.com>
2015-04-02 16:52:25 +0200

    using node-crontab

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-var packagejson = require('./package.json');
+var crontab = require('node-crontab');
 
-exports.version = packagejson.version;
+var twitter = require('./lib/jobs/twitter');
+// var googleplus = require('./lib/jobs/googleplus');
+// var facebook = require('./lib/jobs/facebook');
+
+// Post a random talk on social networks twice a day
+// * One for european time range
+// * Another for USA time range
+var every_6_hours = "* * * * * *";
+var rtt = crontab.scheduleJob(every_6_hours, twitter.randTalkStatus);
+/*
+var rtf = crontab.scheduleJob(every_6_hours, twitter.randTalkFacebook);
+var rtg = crontab.scheduleJob(every_6_hours, twitter.randTalkGooglePlus);
+*/
