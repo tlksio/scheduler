5533e11229e11eb0c766f29bd011702f47f7817f
Raül Pérez <repejota@gmail.com>
2015-04-02 20:52:36 +0200

    Add info status response

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var crontab = require('node-crontab');
 
+var config = require('./config.json');
+
 var twitter = require('./lib/jobs/twitter');
 // var googleplus = require('./lib/jobs/googleplus');
 // var facebook = require('./lib/jobs/facebook');
@@ -8,8 +10,15 @@ var twitter = require('./lib/jobs/twitter');
 // * One for european time range
 // * Another for USA time range
 var every_6_hours = "* * * * * *";
-var rtt = crontab.scheduleJob(every_6_hours, twitter.randTalkStatus);
+var context = { count: 0 };
+var rtt = crontab.scheduleJob(every_6_hours, twitter.randTalkStatus, null, context);
 /*
 var rtf = crontab.scheduleJob(every_6_hours, twitter.randTalkFacebook);
 var rtg = crontab.scheduleJob(every_6_hours, twitter.randTalkGooglePlus);
 */
+
+// TODO: it should be a webserver
+// * exports an API
+// * monitor scheduled jobs
+// * pause / unpause jobs
+// * reload configuration
